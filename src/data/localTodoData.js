de3e34cc4e67ce1344c0bb6a99e9f78b9ee1a98c
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-04-25 23:48:45 +0200

    adding localTodoData

---
+++ src/data/localTodoData.js
@@ -0,0 +1,21 @@
+export const localTodoData = [
+  {
+    id: 1,
+    title: "First todo",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos, reprehenderit id esse eligendi suscipit dolorum, asperiores, neque reiciendis harum fugit cupiditate provident hic maxime officia iusto iure soluta possimus maiores.",
+    priority: "low",
+    completed: false,
+    isEditing: false,
+  },
+
+  {
+    id: 2,
+    title: "Second todo",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos, reprehenderit id esse eligendi suscipit dolorum, asperiores, neque reiciendis harum fugit cupiditate provident hic maxime officia iusto iure soluta possimus maiores.",
+    priority: "medium",
+    completed: false,
+    isEditing: false,
+  },
+];
