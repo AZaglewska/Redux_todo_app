ca4d4e738730da905ba98de78ad2e470b060a6c3
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-04-25 23:46:08 +0200

    creating root component and adding Router to root

---
+++ src/root/Root.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Router from "../routing/Router";
+
+const Root = () => {
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <Router />
+    </div>
+  );
+};
+
+export default Root;
