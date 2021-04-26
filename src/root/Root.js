79b5051247f78d0557ffbc22da36f7605f74f61a
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-04-26 21:07:38 +0200

    adding GlobalStyles to root

--- src/root/Root.js
+++ src/root/Root.js
@@ -1,9 +1,11 @@
 import React from "react";
+import GlobalStyle from "../GlobalStyles/GlobalStyle";
 import Router from "../routing/Router";
 
 const Root = () => {
   return (
     <div>
+      <GlobalStyle />
       <h1>Todo App</h1>
       <Router />
     </div>
