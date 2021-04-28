6accf755554514c39445866fb5ce91c519564d5a
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-04-28 13:38:24 +0200

    adding themes

---
+++ src/GlobalStyles/mainTheme.js
@@ -0,0 +1,28 @@
+export const mainTheme = {
+  colors: {
+    dark: "#626262",
+    lightGrey: "#aeaeae",
+    light: "rgba(255, 255, 255, 0.7)",
+  },
+
+  fontSizes: {
+    xxs: "8px",
+    xs: "12px",
+    s: "16px",
+    m: "20px",
+    l: "24px",
+    xl: "30px",
+    xxl: "50px",
+  },
+
+  fontWeight: {
+    light: 100,
+    medium: 300,
+    semiBold: 400,
+    bold: 500,
+  },
+
+  fontFamily: {
+    mainFont: "Roboto",
+  },
+};
