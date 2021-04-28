5471a2e6131a1bd125005cec16f356681cb47928
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-04-28 13:39:17 +0200

    adding body styles

--- src/GlobalStyles/GlobalStyle.js
+++ src/GlobalStyles/GlobalStyle.js
@@ -9,10 +9,10 @@ box-sizing: border-box;
 }
 
  body{
-    font-family: 'Montserrat', sans-serif;
-    background-color: #efefef;
-
-
+    font-family: 'Roboto', sans-serif;
+    background-image: linear-gradient(to bottom, #d2a5be, #cca3c6, #c3a3cf, #b6a3d7, #a5a5df, #97ade6, #88b4ea, #7abbeb, #7cc8ea, #87d3e7, #99dde3, #aee6e1);
+    height:100vh;
+ 
  
 }
 
