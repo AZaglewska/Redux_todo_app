146e5805a74a78b94a4b38651d05be7304cdb4df
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-05-04 23:44:22 +0200

    adding TodosWrapper

--- src/views/Todos.js
+++ src/views/Todos.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
 import TodoList from "../components/TodoList/TodoList";
+import todoIcon from "../assets/icons/to-do-list.svg";
+import { TodosWrapper } from "./viewsStyles/TodosStyles";
 
 const Todos = ({ todos }) => {
   return (
     <div>
-      <h1>Todos</h1>
+      <TodosWrapper>
+        <h1>Todos</h1>
+        <img src={todoIcon} />
+      </TodosWrapper>
+
       <TodoList todosData={todos} />
     </div>
   );
