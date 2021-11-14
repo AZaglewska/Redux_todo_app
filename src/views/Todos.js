6ed5e86217380fd73f16285efb02a9ea0249fa4f
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-11-14 20:08:09 +0100

    modify imports

--- src/views/Todos.js
+++ src/views/Todos.js
@@ -1,18 +1,19 @@
-import React from "react";
-import { connect } from "react-redux";
-import TodoList from "../components/TodoList/TodoList";
-import todoIcon from "../assets/icons/to-do-list.svg";
-import { TodosWrapper } from "./viewsStyles/TodosStyles";
+import React from 'react';
+import { connect } from 'react-redux';
+import TodoList from '../components/TodoList/TodoList';
+import todoIcon from '../assets/icons/to-do-list.svg';
+import { TodosWrapper } from './viewsStyles/TodosStyles';
+import { todosTypes } from '../helpers/todosTypes';
 
 const Todos = ({ todos }) => {
   return (
     <div>
       <TodosWrapper>
         <h1>Todos</h1>
-        <img src={todoIcon} />
+        <img src={todoIcon} alt="Todo icon" />
       </TodosWrapper>
 
-      <TodoList todosData={todos} />
+      <TodoList todosData={todos} todosType={todosTypes.todos} />
     </div>
   );
 };
