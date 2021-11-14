44613254e13bf746913570709c7e4552818621c7
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-11-14 20:15:32 +0100

    modify reducer, adding switch and actions to reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,90 @@
-import { localTodoData } from "../data/localTodoData";
+import { actionsTypes } from '../actions/actionsTypes';
+import {
+  getTodosFromLocalStorage,
+  getCompletedTodosFromLocalStorage,
+} from '../helpers/localStorageGetters';
+import { todosTypes } from '../helpers/todosTypes';
 
 const initialState = {
-  todos: [...localTodoData],
-  completedTodos: [
-    {
-      id: 3,
-      title: "Skonczony todo",
-      content:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos, reprehenderit id esse eligendi suscipit dolorum, asperiores, neque reiciendis harum fugit cupiditate provident hic maxime officia iusto iure soluta possimus maiores.",
-      priority: "medium",
-      completed: true,
-      isEditing: false,
-    },
-  ],
+  todos: getTodosFromLocalStorage(),
+  completedTodos: getCompletedTodosFromLocalStorage(),
 };
 
 const reducer = (state = initialState, action) => {
-  return state;
+  const { type, payload } = action;
+  switch (type) {
+    case actionsTypes.ADD_TODO:
+      return {
+        ...state,
+        todos: [...state.todos, payload],
+      };
+
+    case actionsTypes.DELETE_TODO:
+      const filteredTodos = state.todos.filter((todo) => todo.id !== payload);
+
+      return {
+        ...state,
+        todos: [...filteredTodos],
+      };
+
+    case actionsTypes.TOGGLE_COMPLETE_TODO:
+      const todoToAdd = state[payload.todosType].find(
+        (todo) => todo.id === payload.todoId
+      );
+
+      const todosAfterTodoRemoved = state[payload.todosType].filter(
+        (todo) => todo.id !== payload.todoId
+      );
+
+      const arrayToAdd =
+        payload.todosType === todosTypes.todos
+          ? todosTypes.completedTodos
+          : todosTypes.todos;
+
+      return {
+        ...state,
+        [payload.todosType]: [...todosAfterTodoRemoved],
+        [arrayToAdd]: [...state[arrayToAdd], todoToAdd],
+      };
+
+    case actionsTypes.TOGGLE_IS_EDITING:
+      const mappedTodos = state[payload.todosType].map((todo) => {
+        if (todo.id === payload.todoId) {
+          return {
+            ...todo,
+            isEditing: !todo.isEditing,
+          };
+        } else {
+          return todo;
+        }
+      });
+
+      return {
+        ...state,
+        [payload.todosType]: [...mappedTodos],
+      };
+
+    case actionsTypes.EDIT_TODO:
+      const { todoId, newValue, todosType, keyToEdit } = payload;
+
+      const todosAfterEditing = state[todosType].map((todo) => {
+        if (todo.id === todoId) {
+          return {
+            ...todo,
+            [keyToEdit]: newValue,
+          };
+        } else {
+          return todo;
+        }
+      });
+      return {
+        ...state,
+        [todosType]: [...todosAfterEditing],
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default reducer;
