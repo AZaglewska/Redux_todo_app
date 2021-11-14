6ca1c5056ea21b32fe7ac5048c2f6e1c92b3ae47
Anna Żaglewska <web.annazaglewska@gmail.com>
2021-11-14 20:06:40 +0100

    adding actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,3 +9,26 @@ export const deleteTodo = (todoId) => ({
   type: actionsTypes.DELETE_TODO,
   payload: todoId,
 });
+
+export const toggleCompleteTodo = (todoId, todosType) => ({
+  type: actionsTypes.TOGGLE_COMPLETE_TODO,
+  payload: {
+    todoId,
+    todosType,
+  },
+});
+
+export const toggleIsEditing = (todoId, todosType) => ({
+  type: actionsTypes.TOGGLE_IS_EDITING,
+  payload: { todoId, todosType },
+});
+
+export const toggleIsEditingCompleted = (todoId) => ({
+  type: actionsTypes.TOGGLE_IS_EDITING_COMPLETED,
+  payload: todoId,
+});
+
+export const editTodo = (todoId, newValue, todosType, keyToEdit) => ({
+  type: actionsTypes.EDIT_TODO,
+  payload: { todoId, newValue, todosType, keyToEdit },
+});
